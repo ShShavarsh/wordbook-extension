2fee54c66af0f11c0b89bada7b24b60da5864dda
Sergey Harutyunyan <Sergey.Harutyunyan@helpsystems.com>
2023-04-08 18:16:07 +0400

    Pushing changes

--- src/extension-core/scripts/cs.js
+++ src/extension-core/scripts/cs.js
@@ -1,9 +1,33 @@
 import React from 'react'
+import './content-script.js'
+import useScript from './use-script'
 
 const ContentScript = () => {
+  useScript('./content-script.js')
+
+  let tabID
+
+  chrome.runtime.onMessage.addListener(async (obj, sender, response) => {
+    const { type, videoId, tabId } = obj
+
+    tabID = tabId
+
+    console.log('type', type)
+    console.log('videoId', videoId)
+    console.log('tabId', tabId)
+  })
+
+  // chrome.scripting.executeScript.executeScript({
+  //   target: { tabID },
+  //   files: ['script.js']
+  // })
+  //   .then(() => console.log('script injected'))
+
   return (
-    <div>aaaaaaaaaaaaaaaaaaaaaa</div>
+    <div>aaaaaaaaaaa</div>
   )
 }
 
 export default ContentScript
+
+// <script src="./content-script.js" type="text/javascript" />
